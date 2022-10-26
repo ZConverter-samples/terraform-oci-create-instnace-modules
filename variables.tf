c393ee43c0b615e7a1165b308a66cb5380844513
tykim <tykim@zconverter.com>
2022-10-26 16:22:28 +0900

    variables update

--- variables.tf
+++ variables.tf
@@ -2,7 +2,30 @@ locals {
   instance_type_split = split(".", "${var.instance_type_name}")
   ssh_authorized_keys = var.OS != "Windows" ? var.ssh_public_key != null ? var.ssh_public_key : var.ssh_public_key_file_path != null ? base64encode(file(var.ssh_public_key_file_path)) : null : null
   user_data_file_path = var.user_data_file_path != null ? file(var.user_data_file_path) : null
-  volume_device       = formatlist("/dev/oracleoci/oraclevd%s", ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
+  volume_device = formatlist("/dev/oracleoci/oraclevd%s", [
+    "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+    "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    ]
+  )
+  protocol = {
+    "all" : "all",
+    "icmp" : "1",
+    "tcp" : "6",
+    "udp" : "17",
+    "icmpv6" : "58"
+  }
+
+  ingress_security_list = var.security_list != null ? [
+    for data in var.security_list :
+    data
+    if data.direction == "ingress"
+  ] : null
+
+  egress_security_list = var.security_list != null ? [
+    for data in var.security_list :
+    data
+    if data.direction == "egress"
+  ] : null
 }
 
 variable "region" {
@@ -17,6 +40,20 @@ variable "vm_name" {
 
 variable "subnet_ocid" {
   type = string
+}
+
+variable "security_list" {
+  type = list(object({
+    direction        = optional(string)
+    ethertype        = optional(string)
+    protocol         = optional(string)
+    port_range_min   = optional(string)
+    port_range_max   = optional(string)
+    remote_ip_prefix = optional(string)
+    type             = optional(string)
+    code             = optional(string)
+  }))
+  default = null
 }
 
 variable "compartment_ocid" {
